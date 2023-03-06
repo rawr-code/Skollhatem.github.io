708da68bef42bfe93e1b5a50e5db52a7050758c8
SkollHatem <emmanuelvillegasgonzalez@gmail.com>
2023-03-06 18:16:47 -0400

    add pwa registerType

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,24 @@ import AstroPWA from "@vite-pwa/astro";
 import tailwind from "@astrojs/tailwind";
 
 // Helper imports
-import { manifest } from './src/utils/seoConfig'
+import { manifest } from "./src/utils/seoConfig";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     AstroPWA({
+      registerType: "autoUpdate",
       manifest,
+      workbox: {
+        globDirectory: "dist",
+        globPatterns: [
+          "**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}",
+        ],
+        // Don't fallback on document based (e.g. `/some-page`) requests
+        // This removes an errant console.log message from showing up.
+        navigateFallback: null,
+      },
       devOptions: {
         enabled: true,
       },
