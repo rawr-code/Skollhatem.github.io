17de315ae11c1f036e10e6b9f18b951389ac188d
SkollHatem <emmanuelvillegasgonzalez@gmail.com>
2023-03-06 17:53:01 -0400

    add pwa support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 import { defineConfig } from "astro/config";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+// Helper imports
+import { manifest } from './src/utils/seoConfig'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://skollhatem.github.io/',
-  base: '/',
+  integrations: [
+    tailwind(),
+    AstroPWA({
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+  site: "https://skollhatem.github.io/",
+  base: "/",
 });
