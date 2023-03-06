d54dc448af7bc96f2ac0e6ace56c75d173cad82b
SkollHatem <emmanuelvillegasgonzalez@gmail.com>
2023-03-06 18:03:50 -0400

    add manifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import AstroPWA from "@vite-pwa/astro";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+// Helper imports
+import { manifest } from './src/utils/seoConfig'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     AstroPWA({
+      manifest,
       devOptions: {
         enabled: true,
       },
