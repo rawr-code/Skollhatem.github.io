85e212990bd50f449a10bbf402a6e891e5554fe0
SkollHatem <emmanuelvillegasgonzalez@gmail.com>
2023-03-06 16:48:59 -0400

    add Home Page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,49 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Space Mono", ...defaultTheme.fontFamily.sans],
+        gugi: ["Gugi"],
+      },
+      colors: {
+        primary: "#091F2C",
+        secondary: "#5DD8D0",
+      },
+      animation: {
+        "blink-anim": "blink 1s linear infinite;",
+      },
+      keyframes: {
+        blink: {
+          "0%": {
+            opacity: 0,
+          },
+          "50%": {
+            opacity: 1,
+          },
+          "100%": {
+            opacity: 0,
+          },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        /* For Webkit-based browsers (Chrome, Safari and Opera) */
+        ".scrollbar-hide": {
+          "-ms-overflow-style": "none" /* IE and Edge */,
+          "scrollbar-width": "none" /* Firefox */,
+        },
+        ".scrollbar-hide::-webkit-scrollbar": {
+          display: "none",
+        },
+      });
+    }),
+  ],
 };
