a3ec5381e5e6d2606e59b2cdaeeca9e4612fc83a
SkollHatem <emmanuelvillegasgonzalez@gmail.com>
2023-03-06 17:03:29 -0400

    add gh-pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://SkollHatem.github.io',
+  base: '/Skollhatem.github.io',
 });
